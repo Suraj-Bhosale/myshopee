687f014e112f28275a223a3e669dae9628dd14c8
Suraj Bhosale <sbhosale@korioclinical.com>
2022-11-01 16:41:12 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavigationBar from './Component/Navigation/NavigationBar'
+// import Header from './Component/Header/Header'
+import FooterOne from './Component/Footer/FooterOne/FooterOne'
+import FooterTwo from './Component/Footer/FooterTwo/FooterTwo'
+import Recommendation from './Component/Recommendation/Recommendation'
+import Section from './Component/Section/Section'
+
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavigationBar />
+      <Section />
+      <Recommendation />
+      <FooterOne />
+      <FooterTwo />
+    
+
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes> */}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Component/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Carousel from '../../header/Carousel'
+import SmartProduct from '../../products/SmartProduct'
+
+function Home() {
+  return (
+    <div>
+        <Carousel />
+        <SmartProduct />
+    </div>
+  )
+}
+
+export default Home
